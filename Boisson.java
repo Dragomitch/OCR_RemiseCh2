d9cb6952d8c9549808bfaaf2ec3d89e53f76f69a
Dragomitch <orohimesama@gmail.com>
2014-10-23 00:15:47 +0200

    Suppression de Bar.java qui etait inutile

--- Boisson.java
+++ Boisson.java
@@ -3,6 +3,14 @@ public class Boisson{
 	private final static String BIERE= "biere";
 	private final static String VIN= "vin";
 
+	public Boisson(){
+
+	}
+
+	public boolean estEgalA(Boisson boisson){
+
+	}
+
 	
 
 }
