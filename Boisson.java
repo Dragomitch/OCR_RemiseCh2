442dfb3c8325b1a84b4250c8a1b70dff85a816c2
Dragomitch <orohimesama@gmail.com>
2014-10-23 00:59:04 +0200

    Ajout du fichier d'Exception ArgumentInvalideException.java . Ecriture de la classe Boisson finie

--- Boisson.java
+++ Boisson.java
@@ -6,46 +6,57 @@ public class Boisson{
 	private double contenance;
 	private int stock;
 
-	public Boisson(String nom, double contenance, String typeDeBoisson){
-
+	public Boisson(String nom, double contenance, String typeDeBoisson)throws ArgumentInvalideException{
+		if(nom== null || nom.equals("") ||contenance>= 0.25 || typeDeBoisson== null|| typeDeBoisson.equals(""))
+			throw new ArgumentInvalideException("Paramètres invalides");
+		this.nom= nom;
+		this.contenance= contenance;
+		this.typeDeBoisson= typeDeBoisson;
 	}
 
-	public Boisson(String nom, double contenance, String typeDeBoisson, int stock){
-
+	public Boisson(String nom, double contenance, String typeDeBoisson, int stock)throws ArgumentInvalideException{
+		this(nom, contenance, typeDeBoisson);
+		if(stock<= 0)
+			throw new ArgumentInvalideException("La quantite doit être >0");
+		this.stock= stock;
 	}
 
 	public boolean estEgalA(Boisson boisson){
-
+		if(boisson.nom == nom && boisson.contenance== contenance)
+			return true;
+		return false;
 	}
 
 	public String getNom(){
-
+		return nom;
 	}
 
 	public String getTypeBoisson(){
-
+		return typeDeBoisson;
 	}
 
 	public double getContenance(){
-
+		return contenance;
 	}
 
-	public int getNbrBouteillesStock(){
-
+	public int getStock(){
+		return stock;
 	}
 
-	public void ajouter(int nb){
-
+	public void ajouter(int nb)throws ArgumentInvalideException{
+		if(nb<= 0) 
+			throw new ArgumentInvalideException("Le nombre à ajouter doit être > 0");
+		stock+= nb;
 	}
 
-	public void retirer(int nb){
-
+	public void retirer(int nb)throws ArgumentInvalideException{
+		if(nb<= 0|| stock-nb <=0) 
+			throw new ArgumentInvalideException("Le nombre à retirer doit être <0."
+			+"Le nombre restant doit être <=0");
+		stock-= nb;
 	}
 
 	public String toString(){
-
+		return nom+" en "+contenance+ " cl - "+stock+" en stock";
 	}
-
-
-
 }
